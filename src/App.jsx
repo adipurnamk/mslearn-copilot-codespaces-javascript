37fc7ed8ed81a6831c783820fba8931b96754c42
adipurnamk <muhammad.adipurna.k@gmail.com>
2025-01-12 15:14:29 +0000

    Update personal information in App.jsx and add hover animation to social icons

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Adipurna",
+  title: "SRE | DevOps Engineer",
+  email: "muhammad.adipurna.k@gmail.com",
+  gitHub: "adipurnamk",
+  instagram: "adipurna.mk",
+  linkedIn: "adipurnamk",
+  medium: "adipurnamk",
+  twitter: "adipurnamk",
+  youTube: "adipurnamk",
 };
 
 const primaryColor = "#4E567E";
